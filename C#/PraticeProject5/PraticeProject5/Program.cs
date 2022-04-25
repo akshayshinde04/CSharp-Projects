6cceff1f2a20fd69b7f181d95f7d5d29d7193ac6
captjack-4 <90202533+captjack-4@users.noreply.github.com>
2022-04-25 09:19:33 +0530

    Update Program.cs

--- C#/PraticeProject5/PraticeProject5/Program.cs
+++ C#/PraticeProject5/PraticeProject5/Program.cs
@@ -32,3 +32,41 @@ namespace PraticeProject5
 
     }
 }
+
+
+//*******************************//
+namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2, Result; char operations;
+            Console.WriteLine("Enter number1: "); number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number2: "); number2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("choose operation ");
+            Console.WriteLine("1 Addition"); Console.WriteLine("2 substraction");
+            Console.WriteLine("3 Multiplication"); Console.WriteLine("4 Division");
+            operations = char.Parse(Console.ReadLine());
+            switch (operations)
+            {
+                case '1':
+                    Result = number1 + number2; Console.WriteLine("Addition of two number is ={0}", Result);
+                    break;
+                case '2':
+                    Result = number1 - number2; Console.WriteLine("Substraction of two number is ={0}", Result);
+                    break;
+                case '3':
+                    Result = number1 * number2; Console.WriteLine("Multiplication of two number is ={0}", Result);
+                    break;
+                case '4':
+                    Result = number1 / number2; Console.WriteLine("Division of two number is ={0}", Result);
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+
+            }
+        }
+    }
+}
