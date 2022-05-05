0106afe5be15db305d40ee2bb5fe3b103ff363cb
Akshay Shinde <a.shinde1@zensar.com>
2022-05-05 10:02:28 +0530

    CodebaseTest1

---
+++ C#/CodeBase Test 1/CodeBase Test 1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace codebasetest_1
+{
+    internal class program
+    {
+        class Passanger
+        {
+            public String Name { get; set; }
+            public int Age { get; set; }
+            Ticket Ticket { get; set; }
+            public Passanger(String name, int age)
+            {
+                Name = name;
+                Age = age;
+                Ticket = new Ticket();
+            }
+            public void TicketBooking(int tickets)
+            {
+                Ticket.Tickets = tickets;
+                Console.WriteLine("Ticket Booked Successfully");
+
+
+
+            }
+        }
+        class Ticket
+        {
+            public int tickets;
+            public int Tickets
+
+
+
+            {
+                get => tickets;
+                set
+                {
+                    if (value > 5)
+                        throw new Exception("Cannot book more than 5 tickets");
+                    else
+                        tickets = value;
+                }
+
+
+
+            }
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            Passanger passanger = new Passanger("Akshay", 5);
+            Console.Write("Enter the number of tickets you want to book:");
+            passanger.TicketBooking(int.Parse(Console.ReadLine()));
+            Console.ReadKey();
+        }
+    }
+}
--- C#/CodeBase Test 1/CodeBase Test 1/Properties/AssemblyInfo.cs
+++ C#/CodeBase Test 1/CodeBase Test 1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Assignment5")]
+[assembly: AssemblyTitle("CodeBase Test 1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Assignment5")]
+[assembly: AssemblyProduct("CodeBase Test 1")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("48a98e9f-4ca9-4a63-b981-ef017bdb56ec")]
+[assembly: Guid("ccee4c2d-48c1-4c23-a85f-42890c29eda2")]
 
 // Version information for an assembly consists of the following four values:
 //
